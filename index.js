08440825dc35904516e92796b292f053b8edf138
rizvifarhan <116972172+rizvifarhan@users.noreply.github.com>
2023-12-14 15:40:55 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+function openhtml() {
+    let url = "https://developer.mozilla.org/en-US/docs/Web/HTML";
+        let opennewtab = open(url,"","height=900px","width=900px");
+}
+
+function openjs() {
+    let url = "https://developer.mozilla.org/en-US/docs/Web/JavaScript";
+        let opennewtab = open(url,"","height=900px","width=900px");
+}
+
+
+function opencss() {
+    let url = "https://developer.mozilla.org/en-US/docs/Web/CSS";
+        let opennewtab = open(url,"","height=900px","width=900px");
+}
+
+function openinsta() {
+    let url = "https://www.instagram.com/farhanrizvi02?igshid=YTQwZjQ0NmI0OA==";
+    let opennewtab = open(url,"","height=900px","width=900px");
+}
